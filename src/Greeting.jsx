254ec9736d9327315a7bb600a5cbf17a7cfbdfd1
Umarali <umaraliomorov13@gmail.com>
2025-01-29 22:48:03 +0600

    four-hw

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,9 +1,19 @@
-export const Greeting = ({picture,textName,tovarName,priceFirst,priceSecond}) => {    
+import { useState } from "react"
+
+export const Greeting = ({picture,textName,tovarName,priceFirst,priceSecond}) => {   
+    
+    const [count, setCount] = useState(0);
+    
     return (
         <div className="card">
             <img src={picture} alt={picture} />
             <p>{textName}</p>
             <span>{tovarName}</span>
             <span className="span-in-span">{priceFirst} <span>{priceSecond}</span></span>
+            <div className="counter">
+                <button onClick={() => setCount((prev) => prev - 1)} className="minus">-</button>
+                <span className="zero">{count}</span>
+                <button onClick={() => setCount((prev) => prev + 1)} className="plus">+</button>
+            </div>
         </div>
 )}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,25 +1,78 @@
 import search from "./images/search.png"
 import heart from "./images/favorite.png"
 import cart from "./images/shopping_cart.png"
+import { useState } from "react";
+const body = document.querySelector("body")
 
 export function Header(){
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => {
+        setIsModalOpen(true);
+        body.style.overflowY = "hidden"
+    };
+    const closeModal = () => {
+        setIsModalOpen(false);
+        body.style.overflowY = "scroll"
+    };
+
+    const [isFavoriteOpen, setIsFavoriteOpen] = useState(false);
+    const openFavorite = () => {
+        setIsFavoriteOpen(true);
+        body.style.overflowY = "hidden"
+    };
+    const closeFavorite = () => {
+        setIsFavoriteOpen(false);
+        body.style.overflowY = "scroll"
+    }
+
+
     return (
         <header className="header">
             <section className="header-section">
-                <h2 className="logo">YOUR LOGO</h2>
-                <ul className="nav">
-                    <li>HOME</li>
-                    <li>SHOP</li>
-                    <li>LOOKBOOK</li>
-                    <li>FEATURES</li>
-                    <li>PAGES</li>
-                    <li>BLOG</li>
-                </ul>
-                <div className="menu">
-                    <img src={search} alt="search" />
-                    <img src={heart} alt="heart" />
-                    <img src={cart} alt="cart" />
+                <div className="header-container">
+                    <h2 className="logo">YOUR LOGO</h2>
+                    <ul className="nav">
+                        <li>HOME</li>
+                        <li>SHOP</li>
+                        <li>LOOKBOOK</li>
+                        <li>FEATURES</li>
+                        <li>PAGES</li>
+                        <li>BLOG</li>
+                    </ul>
+                    <div className="menu">
+                        <button id = "search-nav"></button>
+                        <button onClick={openFavorite} id = "heart-nav"></button>
+                        <button id = "cart-nav"></button>
+                    </div>
+                    <button onClick={openModal} className="login-main">Login</button>
                 </div>
+                {isModalOpen ? (
+                <div className="container-login">
+                    <div className="login-backround">
+                        <h1>Login in your Account</h1>
+                        <button id="x" onClick={closeModal}>x</button>
+                        <hr />
+                        <input type="text" placeholder="Enter your Name"/>
+                        <input type="text" placeholder="Enter your password"/>
+                        <div className={`login-register`}>
+                            <button>Login</button>
+                            <button id="register">Register</button>
+                        </div>
+                    </div>
+                </div>
+                ): null}
+                {isFavoriteOpen ? (
+                <div className="favorite-modal">
+                    <div className ="favorite-backround">
+                        <div className="my-favorites"></div>
+                        <div className="favorite-back">
+                            <hr />
+                            <button onClick={closeFavorite}>Back</button>
+                        </div>
+                    </div>
+                </div>
+                ) : null}
             </section>
             <section className="hero-section">
                 <div className="hero-content">
